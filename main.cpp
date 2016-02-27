23e8fdab0f22330691fc2649c83232af969b16fe
Vladimir Strisovsky <vstrisovsky@gmail.com>
2016-02-27 21:04:34 +0100

    implement expression templates

--- main.cpp
+++ main.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
 #include <tuple>
+#include <functional>
 
-template<typename ... _T>
+template<typename _Cs, typename ... _T>
 struct _Switch
 {
-    std::tuple<const _T& ...> mValues;
-    _Switch(const _T& ... values)
-    : mValues(values ...)
+    _Cs mCases;
+    _Switch(_Cs && cases)
+    : mCases(cases)
+    {
+    }
+
+    void operator()(const _T& ... values)
     {
 
     }
 };
 
 template<typename ... _T>
-_Switch<_T...> _switch(const _T& ... values)
+struct _Switch<void, _T ...>
 {
-    return _Switch<_T...>(values ...);
+    _Switch()
+    {
+    }
+};
+
+template<typename ... _T>
+inline constexpr
+_Switch<void, _T...> _switch()
+{
+    return _Switch<void, _T...>();
 }
 
 template<typename _Fnc, typename ... _T>
@@ -27,6 +41,11 @@ struct _Case
     : mFnc(fnc),
       mValues(values)
     {}
+
+    bool operator()(_T&& ... values)
+    {
+        return true;
+    }
 };
 
 template<typename ... _T>
@@ -45,26 +64,35 @@ struct _Case<void, _T...>
 };
 
 template<typename ... _T>
+inline constexpr
 _Case<void, _T...> _case(_T && ... values)
 {
     return _Case<void, _T...>(values ...);
 }
 
-template<typename _Fnc, typename ... _T>
-_Switch<_T ...> operator <= (_Switch<_T ...>&& s, _Case<_Fnc, _T ...>&& c)
+template<typename _Cs, typename _Fnc, typename ... _T>
+inline constexpr
+auto operator <= (_Switch<_Cs, _T ...>&& s, _Case<_Fnc, _T ...>&& c) ->
+    _Switch<decltype(std::tuple_cat(std::move(s.mCases), std::tuple<_Case<_Fnc, _T ...>>(std::forward<_Case<_Fnc, _T ...>>(c)))), _T ...>
 {
-    return std::move(s);
+    using _Result =  _Switch<decltype(std::tuple_cat(std::move(s.mCases), std::tuple<_Case<_Fnc, _T ...>>(std::forward<_Case<_Fnc, _T ...>>(c)))), _T ...>;
+    return _Result(std::tuple_cat(std::move(s.mCases), std::tuple<_Case<_Fnc, _T ...>>(std::forward<_Case<_Fnc, _T ...>>(c))));
 }
 
+template<typename _Fnc, typename ... _T>
+inline constexpr
+_Switch<std::tuple<_Case<_Fnc, _T ...>>, _T ...> operator <= (_Switch<void, _T ...>&& s, _Case<_Fnc, _T ...>&& c)
+{
+    return _Switch<std::tuple<_Case<_Fnc, _T ...>>, _T ...>(std::tuple<_Case<_Fnc, _T ...>>(std::forward<_Case<_Fnc, _T ...>>(c)));
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-
     int x = 1, y = 3;
-    _switch(x,y)
-            <= _case(1,0)([](int, int){})
-            <= _case(1,1)([](int, int){});
+    auto a = _switch<int, int>()
+            <= _case(1,0)([](int, int){std::cout << "Hello, World 1-0!" << std::endl;})
+            <= _case(1,1)([](int, int){std::cout << "Hello, World 1-1" << std::endl;});
 
+    a(x,y);
     return 0;
 }
